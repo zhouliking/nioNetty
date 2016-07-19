3bf43128bed3593bd094c85750626672444603dd
zhouli <545289437@qq.com>
2016-07-19 14:22:00 +0800

    commit

--- src/com/zl/pool/Worker.java
+++ src/com/zl/pool/Worker.java
@@ -9,7 +9,7 @@ import java.nio.channels.SocketChannel;
 public interface Worker {
 	
 	/**
-	 * 加入一个新的客户端会话
+	 *  加入一个新的客户端会话
 	 * @param channel
 	 */
 	public void registerNewChannelTask(SocketChannel channel);
